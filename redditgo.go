856ef016b21804fecb1e08fecbcf8189bcc6fd3e
Dmitri Panfilov <dimapanfilov@Dmitris-MacBook-Air.local>
2021-05-28 19:37:50 -0400

    Reddit db implementation

---
+++ redditgo.go
@@ -0,0 +1,32 @@
+package redditgo
+
+import "github.com/google/uuid"
+
+type Thread struct {
+	ID          uuid.UUID `db:"id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+}
+
+type Post struct {
+	ID       uuid.UUID `db:"id"`
+	ThreadID uuid.UUID `db:"thread_id"`
+	Title    string    `db:"content"`
+	Content  string    `db:"content"`
+	Votes    int       `db:"votes"`
+}
+
+type Comment struct {
+	ID      uuid.UUID `db:"id"`
+	PostID  uuid.UUID `db:"post_id"`
+	Content string    `db:"content"`
+	Votes   int       `db:"votes"`
+}
+
+type ThreadStore interface {
+	Thread(id uuid.UUID) (Thread, error)
+	Threads() ([]Thread, error)
+	CreateThread(t *Thread) error
+	UpdateThread(t *Thread) error
+	DeleteThread(id uuid.UUID) error
+}
